c2ca57c2e923f6e4cdae2c4c222c3aa4ecf79a98
azeez-shaik2807 <aziz.smartwood@gmail.com>
2025-11-01 13:48:42 +0530

    import function

--- day-6-import/main.tf
+++ day-6-import/main.tf
@@ -12,4 +12,6 @@ resource "aws_instance" "name" {
 # terraform import aws_instance.name i-0cd1d77ae558c4257
 # terraform import aws_s3_bucket.name bucket-name
 
-# always create empty resource then fill in all the required information like name, id, type, ....and terraform plan untill you see no changes to proceed, so you can match the desired changes with the required changes
+# always create empty resource then fill in all the required information like name, id, type, ....and 
+# terraform plan untill you see no changes to proceed, so you can match the desired changes with the required changes
+# main.tf resources should match with all the details in terraform.tfstate so there will be no changes
